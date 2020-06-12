ef51e3e69927bfbd40364f4c505909dbdef45eb5
YruDumb <louista91@hotmail.fr>
2020-06-12 18:57:46 +0200

    R frr

--- src/View/MainMenu/MainMenuView.java
+++ src/View/MainMenu/MainMenuView.java
@@ -3,6 +3,7 @@ package View.MainMenu;
 import Models.MainMenuModel;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -13,7 +14,7 @@ public class MainMenuView extends JPanel implements Observer {
     public MainMenuView(MainMenuModel mainMenuModel) {
         this.mainMenuModel = mainMenuModel;
         this.mainMenuModel.addObserver(this);
-
+        this.mainMenuModel.setBackgroundColor(new Color(255, 0, 0));
         add(new JLabel("Balorant !"));
         this.setBackground(this.mainMenuModel.getBackgroundColor());
     }
