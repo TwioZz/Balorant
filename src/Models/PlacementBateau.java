5121613228b73f5a855391460b34d8ab82892d5a
Valentin le lamentin <echaubard.valentin@gmail.com>
2020-06-18 18:34:14 +0200

    PlacementBateau comment

--- src/Models/PlacementBateau.java
+++ src/Models/PlacementBateau.java
@@ -5,14 +5,25 @@ import java.util.Observable;
 public class PlacementBateau extends Observable {
     private String placement;
 
+    /**
+     * Défini l'orientation du bateau en horizontal par défaut
+     */
     public PlacementBateau() {
         this.placement = Constants.PLACEMENT_HORIZONTAL;
     }
 
+    /**
+     *
+     * @return L'orientation du bateau
+     */
     public String getPlacement() {
         return placement;
     }
 
+    /**
+     * Change l'orientation du bateau
+     * @param placement Nouvelle orientation
+     */
     public void setPlacement(String placement) {
         this.placement = placement;
         setChanged();
